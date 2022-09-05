b7720f50f83103b87f1f0d9da1e8ca268df3f911
Peter Hurford <peter@peterhurford.com>
2022-09-04 22:12:38 -0500

    Setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='squigglepy',  
+     version='0.1',
+     author='Peter Hurford',
+     author_email='peter@peterhurford.com',
+     description='Squiggle programming language for intuitive probabilistic estimation features in Python',
+     long_description=long_description,
+	 long_description_content_type='text/markdown',
+     url='https://github.com/peterhurford/squigglepy',
+     packages=setuptools.find_packages(),
+     classifiers=[
+         'Development Status :: 3 - Alpha',
+         'Programming Language :: Python :: 3',
+         'License :: OSI Approved :: MIT License',
+         'Operating System :: OS Independent',
+     ],
+ )
