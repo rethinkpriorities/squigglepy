656285df10fb86f191da9ef5086d05f01abf4fd3
bcbernardo <bernardo@rethinkpriorities.org>
2023-10-19 12:48:54 -0300

    revert: Revert vectorized np binary search for `_run_mixture()`
    This reverts commit 86b00e7aa169c1c3c9d236e44578c6c937e60714.

--- squigglepy/samplers.py
+++ squigglepy/samplers.py
@@ -587,7 +587,7 @@ def _mixture_sample_for_large_n(
 
     def _run_mixture(picker, i, pbar):
         _tick_tqdm(pbar, _multicore_tqdm_cores)
-        index = np.searchsorted(weights, picker, side='left')
+        index = bisect.bisect_left(weights, picker)
         return values[index][i]
 
     weights = np.cumsum(weights)
