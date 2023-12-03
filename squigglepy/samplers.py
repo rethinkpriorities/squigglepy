f72ca3cdeb91dbf240c4a353c6a2e94a1fd07789
Michael Dickens <mdickens93@gmail.com>
2023-12-03 14:07:50 -0800

    analytic: simplify before sampling

--- squigglepy/samplers.py
+++ squigglepy/samplers.py
@@ -39,6 +39,7 @@ from .distributions import (
     LognormalDistribution,
     MixtureDistribution,
     NormalDistribution,
+    OperableDistribution,
     ParetoDistribution,
     PoissonDistribution,
     TDistribution,
@@ -876,6 +877,10 @@ def sample(
 
     if verbose is None:
         verbose = n >= 1000000
+
+    # Simplify distribution analytically before sampling
+    if isinstance(dist, OperableDistribution):
+        dist = dist.simplify()
 
     # Handle loading from cache
     samples = None
